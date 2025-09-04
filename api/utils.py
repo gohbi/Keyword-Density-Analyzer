3e4c6aacaeb77b2b87a0fb76c06d2305c7e0833f
Reanna Francis <187709664+gohbi@users.noreply.github.com>
2025-09-04 19:24:51 -0400

    Implement caching for spaCy Language object
    Add caching for spaCy Language object to improve performance.

--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,19 @@ import subprocess
 import pathlib
 import os
 import logging
+from functools import lru_cache
+
+# The model is already present in the image (see Dockerfile).
+# We only need to load it; no download, no --dest flag.
+@lru_cache(maxsize=1)
+def get_spacy_nlp():
+    """
+    Return a cached spaCy Language object.
+    The model was baked into the container at build time, so we simply load it.
+    """
+    # Because we set SPACY_DATA in the Dockerfile, spacy.load will find the model.
+    return spacy.load("en_core_web_sm")
+
 
 MODEL_DIR = pathlib.Path(os.getenv("SPACY_MODEL_DIR", "/app/api/_spacy_models"))
 
